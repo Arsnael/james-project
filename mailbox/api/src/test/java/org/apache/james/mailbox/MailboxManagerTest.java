bc1048467a7a526a0c887c65847ea795323be1b7
Rene Cordier <rcordier@linagora.com>
2019-11-12 09:16:26 +0700

    fixup! [Refactoring] Moving remaining tests in mailbox api to JUnit 5

--- mailbox/api/src/test/java/org/apache/james/mailbox/MailboxManagerTest.java
+++ mailbox/api/src/test/java/org/apache/james/mailbox/MailboxManagerTest.java
@@ -144,7 +144,7 @@ public abstract class MailboxManagerTest<T extends MailboxManager> {
     }
 
     @Test
-    void creatingConcurrentlyMailboxesWithSameParentShouldNotFail() throws Exception {
+    public void creatingConcurrentlyMailboxesWithSameParentShouldNotFail() throws Exception {
         MailboxSession session = mailboxManager.createSystemSession(USER_1);
         String mailboxName = "a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z";
 
