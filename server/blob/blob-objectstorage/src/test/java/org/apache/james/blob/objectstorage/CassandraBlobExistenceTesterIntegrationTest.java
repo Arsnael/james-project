525363a78fe73eb9024bce9af7b87773bd2deaec
Rene Cordier <rcordier@linagora.com>
2019-12-19 17:44:49 +0700

    JAMES-3008 Implement more integration tests for CassandraBlobExistenceTesterIntegrationTest

--- server/blob/blob-objectstorage/src/test/java/org/apache/james/blob/objectstorage/CassandraBlobExistenceTesterIntegrationTest.java
+++ server/blob/blob-objectstorage/src/test/java/org/apache/james/blob/objectstorage/CassandraBlobExistenceTesterIntegrationTest.java
@@ -22,6 +22,7 @@ package org.apache.james.blob.objectstorage;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 
 import org.apache.james.backends.cassandra.CassandraCluster;
@@ -40,16 +41,20 @@ import org.apache.james.blob.objectstorage.cassandra.CassandraBlobExistenceModul
 import org.apache.james.blob.objectstorage.cassandra.CassandraBlobExistenceTester;
 import org.apache.james.blob.objectstorage.cassandra.CassandraBlobExistenceTesterDAO;
 import org.apache.james.util.concurrency.ConcurrentTestRunner;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 @ExtendWith(DockerAwsS3Extension.class)
 class CassandraBlobExistenceTesterIntegrationTest implements MetricableBlobStoreContract {
-    private static final HashBlobId.Factory BLOB_ID_FACTORY = new HashBlobId.Factory();
+    static final HashBlobId.Factory BLOB_ID_FACTORY = new HashBlobId.Factory();
+    static final BucketName CUSTOM = BucketName.of("custom");
+    static final byte[] OTHER_SHORT_BYTEARRAY = "other_bytearray".getBytes(StandardCharsets.UTF_8);
 
     @RegisterExtension
     static CassandraClusterExtension cassandraCluster = new CassandraClusterExtension(CassandraBlobExistenceModule.MODULE);
@@ -107,6 +112,173 @@ class CassandraBlobExistenceTesterIntegrationTest implements MetricableBlobStore
     @Disabled("JAMES-2838 Unstable with scality/S3 impl")
     public void readBytesShouldNotReadPartiallyWhenDeletingConcurrentlyBigBlob() {
 
+    }
+
+    @Test
+    void saveBytesShouldSaveBlobIdInBlobExistenceTester() {
+        BucketName defaultBucketName = testee.getDefaultBucketName();
+
+        BlobId blobId = testee().save(defaultBucketName, SHORT_BYTEARRAY).block();
+
+        assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(defaultBucketName), blobId).block())
+            .isTrue();
+    }
+
+    @Test
+    void saveInputStreamShouldSaveBlobIdInBlobExistenceTester() {
+        BucketName defaultBucketName = testee.getDefaultBucketName();
+
+        BlobId blobId = testee().save(defaultBucketName, new ByteArrayInputStream(SHORT_BYTEARRAY)).block();
+
+        assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(defaultBucketName), blobId).block())
+            .isTrue();
+    }
+
+    @Test
+    void saveBigInputStreamShouldSaveBlobIdInBlobExistenceTester() {
+        BucketName defaultBucketName = testee.getDefaultBucketName();
+
+        BlobId blobId = testee().save(defaultBucketName, new ByteArrayInputStream(TWELVE_MEGABYTES)).block();
+
+        assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(defaultBucketName), blobId).block())
+            .isTrue();
+    }
+
+    @Test
+    void saveSameBlobInDifferentBucketsShouldSaveSameTheirBlobWithDifferentBucketIdInBlobExistenceTester() {
+        BucketName defaultBucketName = testee.getDefaultBucketName();
+
+        BlobId blobId = testee().save(defaultBucketName, SHORT_BYTEARRAY).block();
+        testee().save(CUSTOM, SHORT_BYTEARRAY).block();
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(defaultBucketName), blobId).block())
+                .isTrue();
+            softly.assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(CUSTOM), blobId).block())
+                .isTrue();
+        });
+    }
+
+    @Test
+    void saveShouldBeAbleToSaveBlobIDsOfMultipleDataInBlobExistenceTester() {
+        BucketName defaultBucketName = testee.getDefaultBucketName();
+
+        BlobId blobIdDefault = testee().save(defaultBucketName, SHORT_BYTEARRAY).block();
+        BlobId blobIdCustom = testee().save(CUSTOM, new ByteArrayInputStream(OTHER_SHORT_BYTEARRAY)).block();
+        BlobId blobIdBigStream = testee().save(defaultBucketName, new ByteArrayInputStream(TWELVE_MEGABYTES)).block();
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(defaultBucketName), blobIdDefault).block())
+                .isTrue();
+            softly.assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(CUSTOM), blobIdCustom).block())
+                .isTrue();
+            softly.assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(defaultBucketName), blobIdBigStream).block())
+                .isTrue();
+        });
+    }
+
+    @Test
+    void saveShouldBeAbleToSaveBlobAgainAfterDelete() {
+        BucketName defaultBucketName = testee.getDefaultBucketName();
+
+        BlobId blobId = testee().save(defaultBucketName, SHORT_BYTEARRAY).block();
+
+        testee().delete(defaultBucketName, blobId).block();
+
+        testee().save(defaultBucketName, SHORT_BYTEARRAY).block();
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(defaultBucketName), blobId).block())
+                .isTrue();
+            softly.assertThat(testee().read(defaultBucketName, blobId))
+                .hasSameContentAs(new ByteArrayInputStream(SHORT_BYTEARRAY));
+        });
+
+    }
+
+    @Test
+    void deleteShouldDeleteBlobIdInBlobExistenceTester() {
+        BucketName defaultBucketName = testee.getDefaultBucketName();
+
+        BlobId blobId = testee().save(defaultBucketName, SHORT_BYTEARRAY).block();
+
+        testee().delete(defaultBucketName, blobId).block();
+
+        assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(defaultBucketName), blobId).block())
+            .isFalse();
+    }
+
+    @Test
+    void deleteShouldNotDeleteOtherBlobIdsFromSameBucketInBlobExistenceTester() {
+        BucketName defaultBucketName = testee.getDefaultBucketName();
+
+        BlobId blobIdDefault = testee().save(defaultBucketName, SHORT_BYTEARRAY).block();
+        BlobId blobIdCustom = testee().save(defaultBucketName, OTHER_SHORT_BYTEARRAY).block();
+
+        testee().delete(defaultBucketName, blobIdDefault).block();
+
+        assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(defaultBucketName), blobIdCustom).block())
+            .isTrue();
+    }
+
+    @Test
+    void deleteShouldNotDeleteOtherBlobIdsFromOtherBucketsInBlobExistenceTester() {
+        BucketName defaultBucketName = testee.getDefaultBucketName();
+
+        BlobId blobIdDefault = testee().save(defaultBucketName, SHORT_BYTEARRAY).block();
+        BlobId blobIdCustom = testee().save(CUSTOM, OTHER_SHORT_BYTEARRAY).block();
+
+        testee().delete(defaultBucketName, blobIdDefault).block();
+
+        assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(CUSTOM), blobIdCustom).block())
+            .isTrue();
+    }
+
+    @Test
+    void deleteShouldNotDeleteSameBlobIdsFromOtherBucketsInBlobExistenceTester() {
+        BucketName defaultBucketName = testee.getDefaultBucketName();
+
+        BlobId blobIdDefault = testee().save(defaultBucketName, SHORT_BYTEARRAY).block();
+        BlobId blobIdCustom = testee().save(CUSTOM, SHORT_BYTEARRAY).block();
+
+        testee().delete(defaultBucketName, blobIdDefault).block();
+
+        assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(CUSTOM), blobIdCustom).block())
+            .isTrue();
+    }
+
+    @Test
+    void deleteBucketShouldDeleteAllBlobIdsOfABucketInBlobExistenceTester() {
+        BucketName defaultBucketName = testee.getDefaultBucketName();
+
+        BlobId blobIdDefault = testee().save(defaultBucketName, SHORT_BYTEARRAY).block();
+        BlobId blobIdCustom = testee().save(defaultBucketName, OTHER_SHORT_BYTEARRAY).block();
+
+        testee().deleteBucket(defaultBucketName).block();
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(defaultBucketName), blobIdDefault).block())
+                .isFalse();
+            softly.assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(defaultBucketName), blobIdCustom).block())
+                .isFalse();
+        });
+    }
+
+    @Test
+    void deleteBucketShouldDeleteAllBlobIdsWithDifferentBucketsInBlobExistenceTester() {
+        BucketName defaultBucketName = testee.getDefaultBucketName();
+
+        BlobId blobIdDefault = testee().save(defaultBucketName, SHORT_BYTEARRAY).block();
+        BlobId blobIdCustom = testee().save(CUSTOM, OTHER_SHORT_BYTEARRAY).block();
+
+        testee().deleteBucket(defaultBucketName).block();
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(defaultBucketName), blobIdDefault).block())
+                .isFalse();
+            softly.assertThat(cassandraBlobExistenceTester.exists(ObjectStorageBucketName.of(CUSTOM), blobIdCustom).block())
+                .isFalse();
+        });
     }
 
     @RepeatedTest(100)
